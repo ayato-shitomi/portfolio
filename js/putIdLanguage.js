867eb129268f72e83da34b1133b227f571d70944
Your Name <you@example.com>
2022-07-11 16:24:07 +0900

    使用言語等の経験時間の追加

--- js/putIdLanguage.js
+++ js/putIdLanguage.js
@@ -1,27 +1,119 @@
 const	language = [
-	"Python",
-	"C",
-	"C++",
-	"Go",
-	"HTML CSS",
-	"Java Script",
-	"node.js",
-	"Type Script",
-	"PHP",
-	"Ruby",
-	"Ruby on Rails",
-	"VBS",
-	"Brain Fuck"
+	{
+		lang : "Python",
+		from_year : 2019,
+		from_month : 4,
+		flag : "y"
+	},
+	{
+		lang :"C",
+		from_year : 2021,
+		from_month : 7,
+		flag : ""
+	},
+	{
+		lang : "C++",
+		from_year : 2022,
+		from_month : 5,
+		flag : ""
+	},
+	{
+		lang : "Go",
+		from_year : 2021,
+		from_month : 12,
+		flag : ""
+	},
+	{
+		lang : "HTML CSS",
+		from_year : 2019,
+		from_month : 4,
+		flag : ""
+	},
+	{
+		lang : "Java Script",
+		from_year : 2020,
+		from_month : 4,
+		flag : "y"
+	},
+	{
+		lang : "node.js",
+		from_year : 2021,
+		from_month : 12,
+		flag : ""
+	},
+	{
+		lang : "Type Script",
+		from_year : 2022,
+		from_month : 5,
+		flag : "y"
+	},
+	{
+		lang : "PHP",
+		from_year : 2020,
+		from_month : 4,
+		flag : ""
+	},
+	{
+		lang : "Ruby",
+		from_year : 2022,
+		from_month : 5,
+		flag : ""
+	},
+	{
+		lang : "Ruby on Rails",
+		from_year : 2022,
+		from_month : 5,
+		flag : "y"
+	},
+	{
+		lang : "VBS",
+		from_year : 2019,
+		from_month : 5,
+		flag : ""
+	},
+	{
+		lang : "Brain Fuck",
+		from_year : 2022,
+		from_month : 6,
+		flag : ""
+	}
 ];
 
 function outPutDataset(dataSet, id) {
 	let	parent = document.getElementById(id);
-	dataSet.forEach(item => {
-		var	div = document.createElement("div");
+	let	today = new Date();
+	for (const item of dataSet) {
+		let	langParent = document.createElement("div");
+		let	divLang = document.createElement("div");
+		let	divDate = document.createElement("div");
 		
-		div.innerText = item;
+		langParent.className = "classParent";
+		divLang.className = "classLangName";
+		divDate.className = "classLangDate";
+		let	year = today.getFullYear() - item.from_year;
+		let	month = (today.getMonth() + 1) - item.from_month + year * 12;
+		
+		year = Math.floor(month/12);
+		month = month%12;
 
-		parent.appendChild(div);
-	});
+		let	fromDate;
+		if (year <= 0) {
+			fromDate = month + "ヶ月";
+		} else if (month == 0) {
+			fromDate = year + "年";
+		} else {
+			fromDate = year + "年" + month + "ヶ月";
+		};
+		let	langTxt = item.lang;
+		divLang.innerText = langTxt;
+		if (item.flag == "y") {
+			fromDate = fromDate + "(実務含む)";
+		}
+		divDate.innerText = fromDate;
+
+		langParent.appendChild(divLang);
+		langParent.appendChild(divDate);
+		parent.appendChild(langParent);
+	};
 };
 outPutDataset(language, "idLanguage");
--- js/putIdMajor.js
+++ js/putIdMajor.js
@@ -1,29 +1,125 @@
 const	major = [
-	"Unix開発",
-	"Linux",
-	"Ubuntu",
-	"Debian",
-	"Kali Linux",
-	"Shell",
-	"Google Cloud Platform",
-	"FireBase",
-	"サーバー構築",
-	"ネットワークセキュリティ",
-	"ペネトレーションテスト",
-	"Webページ作成",
-	"Webアプリ開発",
-	"バックエンド",
-	"フロントエンド"
+	{
+		major : "Unix",
+		from_year : 2019,
+		from_month : 4,
+		flag : "y"
+	},
+	{
+		major : "Linux",
+		from_year : 2019,
+		from_month : 4,
+		flag : ""
+	},
+	{
+		major : "Ubuntu",
+		from_year : 2020,
+		from_month : 4,
+		flag : "y"
+	},
+	{
+		major : "Debian",
+		from_year : 2020,
+		from_month : 4,
+		flag : ""
+	},
+	{
+		major : "Kali Linux",
+		from_year : 2019,
+		from_month : 4,
+		flag : ""
+	},
+	{
+		major : "Shell",
+		from_year : 2020,
+		from_month : 4,
+		flag : "y"
+	},
+	{
+		major : "Google Cloud Platform",
+		from_year : 2022,
+		from_month : 4,
+		flag : "y"
+	},
+	{
+		major : "FireBase",
+		from_year : 2022,
+		from_month : 4,
+		flag : "y"
+	},
+	{
+		major : "サーバー構築",
+		from_year : 2022,
+		from_month : 1,
+		flag : ""
+	},
+	{
+		major : "ネットワークセキュリティ",
+		from_year : 2020,
+		from_month : 4,
+		flag : ""
+	},
+	{
+		major : "ペネトレーションテスト",
+		from_year : 2020,
+		from_month : 4,
+		flag : ""
+	},
+	{
+		major : "Webページ作成",
+		from_year : 2020,
+		from_month : 3,
+		flag : ""
+	},
+	{
+		major : "バックエンド",
+		from_year : 2022,
+		from_month : 4,
+		flag : "y"
+	},
+	{
+		major : "フロントエンド",
+		from_year : 2021,
+		from_month : 4,
+		flag : ""
+	}
 ];
 
 function outPutDataset(dataSet, id) {
 	let	parent = document.getElementById(id);
-	dataSet.forEach(item => {
-		var	div = document.createElement("div");
+	let	today = new Date();
+	for (const item of dataSet) {
+		let	langParent = document.createElement("div");
+		let	divLang = document.createElement("div");
+		let	divDate = document.createElement("div");
 		
-		div.innerText = item;
+		langParent.className = "classParent";
+		divLang.className = "classLangName";
+		divDate.className = "classLangDate";
+		let	year = today.getFullYear() - item.from_year;
+		let	month = (today.getMonth() + 1) - item.from_month + year * 12;
+		
+		year = Math.floor(month/12);
+		month = month%12;
 
-		parent.appendChild(div);
-	});
+		let	fromDate;
+		if (year <= 0) {
+			fromDate = month + "ヶ月";
+		} else if (month == 0) {
+			fromDate = year + "年";
+		} else {
+			fromDate = year + "年" + month + "ヶ月";
+		};
+		let	langTxt = item.major;
+		divLang.innerText = langTxt;
+		if (item.flag == "y") {
+			fromDate = fromDate + "(実務含む)";
+		}
+		divDate.innerText = fromDate;
+
+		langParent.appendChild(divLang);
+		langParent.appendChild(divDate);
+		parent.appendChild(langParent);
+	};
 };
 outPutDataset(major, "idMajor");
