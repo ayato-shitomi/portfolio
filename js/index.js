e95ab1a1ebfe49d67c8199504ddb13f450e104cc
LabPixel <74812891+ayato-shitomi@users.noreply.github.com>
2023-07-04 10:55:53 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
+function isSmartPhone() {
+	if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
 function convertTextToHTML(text) {
 	let html = '';
 	let currentTag = '';
-
+	let mobileFlag = isSmartPhone();
+	
 	const lines = text.split('\n');
 	for (let line of lines) {
 		// check if line is empty
@@ -16,7 +26,11 @@ function convertTextToHTML(text) {
 			}
 		} else if (currentTag == "table") {
 			if (!(line.startsWith("| "))) {
-				html += '</table>';
+				if (mobileFlag == false) {
+					html += '</table>';
+				} else {
+					html += '</ul>';
+				}
 				currentTag = '';
 			}
 		}
@@ -38,10 +52,18 @@ function convertTextToHTML(text) {
 			}
 			currentTag = "li";
 		} else if (line.startsWith("| ")) {
-			if (currentTag == "table") {
-				html += `<tr><td>${line.slice(2).split(", ")[0]}</td><td></td><td>${line.slice(2).split(", ")[1]}</td></tr>`;
+			if (mobileFlag == false) {
+				if (currentTag == "table") {
+					html += `<tr><td>${line.slice(2).split(", ")[0]}</td><td></td><td>${line.slice(2).split(", ")[1]}</td></tr>`;
+				} else {
+					html += `<table><tr><td>${line.slice(2).split(", ")[0]}</td><td></td><td>${line.slice(2).split(", ")[1]}</td></tr>`;
+				}
 			} else {
-				html += `<table><tr><td>${line.slice(2).split(", ")[0]}</td><td></td><td>${line.slice(2).split(", ")[1]}</td></tr>`;
+				if (currentTag == "li") {
+					html += `<li>${line.slice(2).split(", ")[0]} ${line.slice(2).split(", ")[1]}</li>`;
+				} else {
+					html += `<ul><li>${line.slice(2).split(", ")[0]} ${line.slice(2).split(", ")[1]}</li>`;
+				}
 			}
 			currentTag = "table";
 		} else {
