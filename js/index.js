3cabd4c274edb7b169f3b204ba30cbef177974d6
LabPixel <74812891+ayato-shitomi@users.noreply.github.com>
2023-07-04 10:01:20 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -51,7 +51,7 @@ function convertTextToHTML(text) {
 	return html;
 }
 
-fetch('./index.txt')
+fetch('./src/index.txt')
 	.then(response => response.text())
 	.then(text => {
 		const html = convertTextToHTML(text);
